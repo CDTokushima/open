0d3ada14ca71f365e1928a89fac8225c09744be6
CoderDojo Tokushima <31502172+CDTokushima@users.noreply.github.com>
2022-10-10 20:07:48 +0900

    Update path.js

--- chatbot/chatbot/path.js
+++ chatbot/chatbot/path.js
@@ -1,2 +1,4 @@
-const RELATIVE_PATH = "/open/chatbot/chatbot";
+//const RELATIVE_PATH = "/chatbot";
+//const RELATIVE_PATH = "/open/chatbot/chatbot";
+const RELATIVE_PATH = "https://cdtokushima.github.io/open/chatbot/chatbot/";
 
